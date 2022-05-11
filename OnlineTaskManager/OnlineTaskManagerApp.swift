10b4ed0f6db64b46e8ba26a3cc4fb02913a988bb
Berkay-Disli <berkaydisli.work@gmail.com>
2022-05-12 00:52:37 +0300

    Initial Commit

---
+++ OnlineTaskManager/OnlineTaskManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnlineTaskManagerApp.swift
+//  OnlineTaskManager
+//
+//  Created by Berkay Disli on 12.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct OnlineTaskManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
